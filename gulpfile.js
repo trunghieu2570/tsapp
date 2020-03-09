f97ce020954587e2c2482cdc6574e4c825751fda
Hiếu Trần Trung <17520484@ms.uit.edu.vn>
2020-03-09 21:11:40 +0700

    add some gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,40 +5,62 @@ var nodemon = require('gulp-nodemon');
 var tsApp = ts.createProject('tsconfig.json');
 
 var paths = {
-    ejs: 'src/views/**/*',
-    scss: 'src/sass/**/*.scss',
-    webfonts: 'src/webfonts/**/*',
+    typescripts:
+    ['src/controllers/**/*.ts',
+     'src/model/**/*.ts',
+     'src/app.ts',
+     'src/server.ts'],
+    views: ['src/views/**/*'],
+    scss: ['src/sass/**/*.scss'],
+    webfonts: ['src/webfonts/**/*'],
 }
 
-
-gulp.task('cpViews', function () {
-    return gulp.src(paths.ejs)
+//copy views
+gulp.task('copy:views', function () {
+    return gulp.src(paths.views)
         .pipe(gulp.dest('dist/views'));
 });
 
-gulp.task('sass', function() {
+//compile sass
+gulp.task('compile:sass', function() {
     return gulp.src(paths.scss)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/public/css'));
 });
 
-gulp.task('sass:watch', function() {
-    return gulp.watch(paths.scss, gulp.series('sass'));
+//compile typescript
+gulp.task('compile:ts', function () {
+    return tsApp.src().pipe(tsApp()).js.pipe(gulp.dest('dist'));
 });
 
 
-gulp.task('compile', gulp.series('cpViews', function () {
-    return tsApp.src().pipe(tsApp()).js.pipe(gulp.dest('dist'));
-}));
+//watch sass
+gulp.task('watch:sass', function() {
+    return gulp.watch(paths.scss, gulp.series('compile:sass'));
+});
 
-gulp.task('watch', gulp.series('compile', function () {
-    return gulp.watch('src/**/*', gulp.series('compile'));
-}));
+gulp.task('watch:copy', function() {
+    return gulp.watch(paths.views, gulp.series('copy:views'));
+});
 
+//watch typescript
+gulp.task('watch:ts', function () {
+    return gulp.watch(paths.typescripts, gulp.series('compile:ts'));
+});
+
+//monitoring
 gulp.task('nodemon', function () {
     return nodemon({
         script: 'dist/app.js',
-    })
+        watch: ['dist'],
+    });
 });
 
-gulp.task('default', gulp.parallel('watch', 'nodemon', 'sass:watch'));
+gulp.task('copy', gulp.parallel('copy:views'));
+
+gulp.task('watch', gulp.parallel('watch:copy', 'watch:ts', 'watch:sass', 'nodemon'));
+
+gulp.task('start', gulp.parallel('copy', 'compile:ts', 'compile:sass'));
+
+//default task
+gulp.task('default', gulp.series('start', 'watch'));
